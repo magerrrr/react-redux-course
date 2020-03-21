58f09cebd2612f00e9fc6af02a1290a980a6b07a
Kirill Mager <kiruha24@mail.ru>
2020-03-21 23:58:05 +0300

    feat: add redux devtools

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { rootReducer } from './redux/rootReducer';
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
 
 const app = (
   <Provider store={store}>
