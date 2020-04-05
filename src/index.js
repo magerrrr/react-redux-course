49b9a043927dc838881d2b2c33873d217524ba81
Kirill Mager <kiruha24@mail.ru>
2020-04-05 15:47:14 +0300

    feat: add redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { compose, createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { compose, createStore } from 'redux';
-import { Provider } from 'react-redux';
 import { rootReducer } from './redux/rootReducer';
 
 const store = createStore(rootReducer, compose(
+    applyMiddleware(
+        thunk
+    ),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-))
+));
 
 const app = (
   <Provider store={store}>
     <App />
   </Provider>
-)
+);
 
 render(app, document.getElementById('root'));
 
